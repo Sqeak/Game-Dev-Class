ae0b074b8b0a2a61375a829e413680ff487e5785
Squeak <63600830+Sqeak@users.noreply.github.com>
2022-05-06 08:22:21 -0700

    Mre updates to Lander Class

--- Defender/main.py
+++ Defender/main.py
@@ -13,6 +13,8 @@ LANDER_HEIGHT = 50
 player_list = arcade.SpriteList()
 enemy_list = []
 
+bullet_list = arcade.SpriteList()
+
 lander_list = arcade.SpriteList()
 mutant_list = arcade.SpriteList()
 baiter_list = arcade.SpriteList()
@@ -88,12 +90,17 @@ class Manti(arcade.Sprite):
         self.velocity = Vector2(0, 0)
         
         self.shot_deviation = 0
+        self.bullet_speed = 0
         
         self.frequency_of_direction_delta = 0
+        self.frequency_of_fire = 0
         self.direction_delta_randomness = 2
+        self.shot_randomness = 4
         
-        self.timer = time.time()
+        self.timer1 = time.time()
+        self.timer2 = time.time()
         self.time_till_next_change = 0
+        self.time_till_next_shot = 0
         
     def update(self):
         self.position += self.velocity
@@ -120,29 +127,53 @@ class Lander(Manti):
         
         self.maxVelocity = 1
         
+        self.shot_deviation = 5
+        self.bullet_speed = 3
+        
         self.velocity.x = random.uniform(-self.maxVelocity, self.maxVelocity)
         self.velocity.y = random.uniform(-self.maxVelocity/2, self.maxVelocity/2)
         
-        self.frequency_of_change_in_direction = 3        
+        self.frequency_of_change_in_direction = 3
+        self.frequency_of_fire = 4
+        
+        self.myhuman = None
             
     def movementPattern(self):
         #I need to find out how to make the lander fly near the ground. I'm thinking that if i have a certain height threshold, then
         #I can have the lander reverse it's y velocity to keep it from getting too high.
         #Of course, only sdo this if it doesn't have a human, but if it does, then beeline for the top of the screen.
         if self.has_human:
-            pass
+            #If it has a human then cut off horizontal thrust and go streight to the top.
+            self.velocity.x = 0
+            self.velocity.y = -self.maxVelocity
+            
+            #If you reach the top of the screen the call the mutate function
+            if self.position.y >= SCREEN_HEIGHT:
+                self.mutate()
         
         else:
             if self.position.y <= self.max_height:
                 self.velocity.y = -self.velocity.y
             
-            if time.time() - self.timer >= self.time_till_next_change:
+            #If movement cooldown is over
+            if time.time() - self.timer1 >= self.time_till_next_change:
+                #Randomly change velocity
                 self.velocity.x = random.uniform(-self.maxVelocity, self.maxVelocity)
                 self.velocity.y = random.uniform(-self.maxVelocity/2, self.maxVelocity/2)
-                self.timer = time.time()
+                #Reset cool down and add or remove a random amount of time to add variety to it's behaviour
+                self.timer1 = time.time()
                 self.time_till_next_change = self.frequency_of_change_in_direction + random.uniform(-self.direction_delta_randomness, self.direction_delta_randomness)
-            
-            
+                
+    def firingPattern(self):
+        #If firing cooldown is over
+        if time.time() - self.timer2 >= self.time_till_next_shot:
+            #Find future position of player and aim in that direction with some inaccuracy
+            bullet_direction = predictLocation(player.position, self.position, self.bullet_speed)
+            bullet_direction += Vector2(random.uniform(-self.shot_deviation, self.shot_deviation), random.uniform(-self.shot_deviation, self.shot_deviation)).normalize()
+            bullet_list.append(Bullet(self.position, bullet_direction, self.bullet_speed))
+            #Reset firing cooldown and add or remove a random amount
+            self.time_till_next_shot = self.frequency_of_fire + random.uniform(-self.shot_randomness, self.shot_randomness)
+                
                 
             
             
